9c86a24b267ac4e96234b59a7959e80bb9a9edcb
maticlikar <matic@likar.me>
2016-06-19 18:56:24 -0700

    started level 1, added ground enemy

---
+++ Assets/scripts/enemy/ground/goombaStomp.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+var animator : Animator;
+var movement : movement;
+var thisEnemy : GameObject;
+var character : GameObject;
+var top : GameObject;
+
+function OnCollisionEnter2D(Collider: Collision2D) {
+	if (Collider.gameObject.tag.Equals("player")){
+		animator.SetBool('isDead', true);
+		movement.isDead = true;
+		Destroy(thisEnemy, 3);
+		Destroy(character.GetComponent.<Collider2D>());
+		Destroy(top.GetComponent.<Collider2D>());
+	}
+}
---
+++ Assets/scripts/enemy/ground/movement.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+var thisEnemyRigid : Rigidbody2D;
+var thisEnemyTransform : Transform;
+var isDead : boolean = false;
+private var walkSpeed : float = 0.6f;
+
+function Update () {
+	if (!isDead)		
+		thisEnemyRigid.velocity.x = walkSpeed;
+	else
+		thisEnemyRigid.velocity.x = 0;
+}
+
+function OnCollisionEnter2D(Collider: Collision2D) {
+	if (Collider.gameObject.tag.Equals("bound")){
+		thisEnemyTransform.localScale.x = thisEnemyTransform.localScale.x * -1;
+		walkSpeed *= -1;
+	}
+}
